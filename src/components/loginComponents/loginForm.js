5c4649575ff7f0dbce35f1348e60db2bd55e79b2
shabazs453 <shabazs453@gmail.com>
2023-10-19 12:42:20 +0530

    input padding adjust login form

--- src/components/loginComponents/loginForm.js
+++ src/components/loginComponents/loginForm.js
@@ -12,13 +12,13 @@ const InputField = ({
   emailValidity,
 }) => (
   <div className="flex-1 flex items-center justify-center mt-8 h-[4.6rem] w-full">
-    <div className="rounded-[1rem] bg-gray-100 shadow-[0px_4px_32px_rgba(0,_0,_0,_0.04)] h-full w-full  flex items-center justify-between py-[1.06rem] px-[1.06rem] gap-[1.06rem] border-[0.5px] border-solid border-text_muted">
+    <div className="rounded-[1rem] bg-bg_input shadow-[0px_4px_32px_rgba(0,_0,_0,_0.04)] h-full w-full  flex items-center justify-between px-[1.06rem] gap-[1.06rem] border-[0.5px] border-solid border-border_primary">
       <img className="relative w-[27px] h-[27px]" alt="" src={iconSrc} />
       {/* <div className="flex-auto flex items-center h-full w-auto"> */}
       <div className="relative bg-text_muted w-[1px] h-[2.25rem]" />
-      <div className="flex-1 h-full relative">
+      <div className="flex-1 h-[2.5rem] relative">
         <label className="h-full">
-          <span className="absolute top-[-0.5rem] left-0 text-[0.75rem] z-[3]">
+          <span className="absolute top-[0rem] left-0 text-[0.75rem] z-[3]">
             {" "}
             {placeholder}
           </span>
@@ -27,7 +27,7 @@ const InputField = ({
             name={name}
             value={value}
             onChange={onChange}
-            className="h-full w-full px-0 pb-0 bg-transparent border-none focus:outline-none focus:outline-offset-0 relative text-[1rem] leading-[1rem] font-semibold font-lexend"
+            className="absolute bottom-0 h-[1.3rem] w-full px-0 py-0 bg-transparent border-none outline-none text-[1rem] leading-[1rem] font-semibold font-lexend"
           />
         </label>
       </div>
@@ -126,7 +126,7 @@ export default function LoginForm() {
           />
 
           <div className="flex flex-col justify-center text-white mt-8 cursor-pointer items-center w-full h-[4rem]">
-            <div className="rounded-[1rem] bg-bg_primary shadow-[8px_8px_16px_#cddbff]  flex items-center justify-center box-border w-full h-full">
+            <div className="rounded-[1rem] bg-bg_primary hover:bg-bg_hover shadow-[8px_8px_16px_#cddbff]  flex items-center justify-center box-border w-full h-full">
               <button
                 onClick={handleSubmit}
                 className="relative bg-transparent border-none outline-none w-full h-full cursor-pointer text-white text-sm font-lexend rounded-[1rem]"
